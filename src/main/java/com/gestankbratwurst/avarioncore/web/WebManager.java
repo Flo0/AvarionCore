3fccd3663c4e48ad00157e0d667783301ecd5c1d
BuildTools <unconfigured@null.spigotmc.org>
2020-07-04 17:13:41 +0200

    Added http 204 response to WebManager

--- src/main/java/com/gestankbratwurst/avarioncore/web/WebManager.java
+++ src/main/java/com/gestankbratwurst/avarioncore/web/WebManager.java
@@ -4,6 +4,7 @@ import com.gestankbratwurst.avarioncore.AvarionCore;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
@@ -30,7 +31,7 @@ public class WebManager {
 
   private final AvarionCore avarionCore;
   private final String responseOk = "HTTP/1.1 204 OK\r\n\r\n";
-  private final String responseDeny = "HTTP/1.1 204 Wrong password\r\n\r\n";
+  private final String responseDeny = "HTTP/1.1 204 Wrong_Password\r\n\r\n";
   private final String password = "043tgnnga9e8hz6imujhhnuhnady94wzhnrzfh09m4w5hsnrnh89er";
 
   private void listenForRestart() {
@@ -41,6 +42,7 @@ public class WebManager {
         final Socket socket = serverSocket.accept();
 
         final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        final OutputStream outputStream = socket.getOutputStream();
         final String line = bufferedReader.readLine();
         final String[] args = line.split(" ");
 
@@ -48,6 +50,7 @@ public class WebManager {
           System.out.println("§aHTTP shutdown initialised.");
           Bukkit.getScheduler().runTask(this.avarionCore, Bukkit::shutdown);
           Bukkit.getServer().shutdown();
+          outputStream.write(this.responseOk.getBytes(StandardCharsets.UTF_8));
           break;
         }
 
